b08b42add14e820b1a2fd242634d3e32e9bfa4dd
P. L. Lim <2090236+pllim@users.noreply.github.com>
2020-10-12 19:08:24 -0400

    Apply review nitpicks

--- astropy/utils/tests/test_data.py
+++ astropy/utils/tests/test_data.py
@@ -1058,17 +1058,17 @@ def test_data_noastropy_fallback(monkeypatch):
 
     assert n_warns in (2, 4), f'Expected 2 or 4 warnings, got {n_warns}'
 
-
-    partial_warn_msgs = ['remote data cache could not be accessed' ,'temporary file']
+    partial_warn_msgs = ['remote data cache could not be accessed', 'temporary file']
     if n_warns == 4:
         partial_warn_msgs.extend(['socket', 'socket'])
 
     for wl in warning_lines:
+        cur_w = str(wl).lower()
         for i, partial_msg in enumerate(partial_warn_msgs):
-            if partial_msg in str(wl).lower():
+            if partial_msg in cur_w:
                 del partial_warn_msgs[i]
                 break
-    assert len(partial_warn_msgs) == 0, 'Did not get some expected warnings:' + str(partial_warn_msgs)
+    assert len(partial_warn_msgs) == 0, f'Got some unexpected warnings: {partial_warn_msgs}'
 
     assert os.path.isfile(fnout)
 
